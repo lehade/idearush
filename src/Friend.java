3bb24fc5d06e41af38c57d196e4e5a3dbaf4c1b6
lehade <alexd89@list.ru>
2018-02-14 00:10:42 +0300

    test commit java

---
+++ src/Friend.java
@@ -0,0 +1,28 @@
+public class Friend {
+
+    String name;
+    int age;
+    char sex;
+
+    public Friend(String name)
+    {
+        this.name = name;
+    }
+
+    public Friend(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Friend(String name, int age, char sex)
+    {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
