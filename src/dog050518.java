86fcc954dea43eb6003fe2e6f2e95a8452952dc8
lehade <alexd89@list.ru>
2018-02-14 23:52:19 +0300

    dog constructor 050518

---
+++ src/dog050518.java
@@ -0,0 +1,28 @@
+public class dog050518 {
+
+    String name;
+    int height;
+    String color;
+
+    public dog050518(String name)
+    {
+        this.name = name;
+    }
+
+    public dog050518(String name, int height)
+    {
+        this.name = name;
+        this.height = height;
+    }
+
+    public dog050518(String name, int height, String color)
+    {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
