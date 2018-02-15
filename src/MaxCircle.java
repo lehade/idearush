4a9cd2bef2c04f6df9a770b8544e49c02cf69715
lehade <alexd89@list.ru>
2018-02-16 00:04:34 +0300

    maxcircle constructor 050522

---
+++ src/MaxCircle.java
@@ -0,0 +1,26 @@
+public class MaxCircle {
+    public double x;
+    public double y;
+    public double radius;
+
+    public MaxCircle (double x) {
+        this.x = x;
+    }
+
+    public MaxCircle (double x, double y) {
+        this.y = y;
+    }
+
+    public MaxCircle (double x, double y, double radius) {
+        this.radius = radius;
+    }
+    public MaxCircle () {
+        this.x = 3;
+        this.y = 4.5;
+        this.radius = 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
