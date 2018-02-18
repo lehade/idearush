e99414a40bd8e6a2b540eb2c39bc041a0b6306fa
lehade <alexd89@list.ru>
2018-02-18 12:00:31 +0300

    sum of 2 ints 0530

---
+++ src/rush0530.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class rush0530 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
