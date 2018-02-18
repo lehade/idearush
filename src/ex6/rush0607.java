a864c2f0a9f489c07c1dbe1969d85d7b499fbc2a
lehade <alexd89@list.ru>
2018-02-18 22:51:34 +0300

    even odd from kb 0605

---
+++ src/ex6/rush0607.java
@@ -0,0 +1,14 @@
+package ex6;
+
+public class rush0607 {
+    private static int catCount =0;
+
+    public rush0607()
+    {
+        rush0607.catCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ex6/rush0608.java
@@ -0,0 +1,8 @@
+package ex6;
+
+public class rush0608 {
+
+    public static void main(String[] args) {
+
+    }
+}
