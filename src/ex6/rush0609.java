cb806a9c93d5e2259d4193ecbbf98c4eac532f18
lehade <alexd89@list.ru>
2018-02-20 00:25:41 +0300

    2 points 0609

---
+++ src/ex6/rush0609.java
@@ -0,0 +1,15 @@
+package ex6;
+
+import static java.lang.Math.sqrt;
+
+public class rush0609 {
+    public static double getDistance(int x1, int y1, int x2, int y2) {
+        //напишите тут ваш код
+
+        return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
