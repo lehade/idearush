1155bce40a277475546d99625cde09379fbd2954
lehade <alexd89@list.ru>
2018-02-20 00:10:45 +0300

    catcount(junk) 0608

--- src/ex6/rush0608.java
+++ src/ex6/rush0608.java
@@ -2,6 +2,24 @@ package ex6;
 
 public class rush0608 {
 
+    private static int catCount = 0;
+
+    public rush0608() {
+        catCount++;
+    }
+
+    public static int getCatCount() {
+        //напишите тут ваш код
+        return catCount;
+
+    }
+
+    public static void setCatCount(int count) {
+        //напишите тут ваш код
+        catCount = count;
+
+    }
+
     public static void main(String[] args) {
 
     }
