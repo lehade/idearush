73feddd220d44e7826622dbdb597a95265d279e8
lehade <alexd89@list.ru>
2018-02-20 23:06:33 +0300

    var string print 0611

---
+++ src/ex6/rush0611.java
@@ -0,0 +1,26 @@
+package ex6;
+
+public class rush0611 {
+
+    public static String multiply(String s) {
+        String result = "";
+        //напишите тут ваш код
+        for (int i = 0; i < 5; i++) {
+
+        }
+        return result;
+    }
+
+    public static String multiply(String s, int count) {
+        String result = "";
+        //напишите тут ваш код
+        for (int i = 0; i < count; i++) {
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
