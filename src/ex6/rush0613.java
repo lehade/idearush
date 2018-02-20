84ac3ff682f561a8cfb03806ad30955d1a2d8339
lehade <alexd89@list.ru>
2018-02-21 00:30:53 +0300

    junk calc 0613

---
+++ src/ex6/rush0613.java
@@ -0,0 +1,33 @@
+package ex6;
+
+public class rush0613 {
+    public static int plus(int a, int b) {
+        //напишите тут ваш код
+
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        //напишите тут ваш код
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        //напишите тут ваш код
+        return a * b;
+    }
+
+    public static double division(int a, int b) {
+        //напишите тут ваш код
+        return (double)a / b;
+    }
+
+    public static double percent(int a, int b) {
+        //напишите тут ваш код
+        return (( double) (a) )/100 * b;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
