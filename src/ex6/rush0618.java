7d2402456e5fb157197263ba54ed6bd9ffc2ab70
lehade <alexd89@list.ru>
2018-02-23 22:31:28 +0300

    junk class print 0618

---
+++ src/ex6/rush0618.java
@@ -0,0 +1,14 @@
+package ex6;
+
+public class rush0618 {
+    public static class KissMyShinyMetalAss {
+
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        KissMyShinyMetalAss s = new KissMyShinyMetalAss();
+
+        System.out.println(s);
+    }
+}
