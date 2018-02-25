6eb8418a321aa2bd3b5f20158a5dfaa4dad3f88d
lehade <alexd89@list.ru>
2018-02-25 23:23:50 +0300

    some array list 10 cats 0614

--- src/ex6/rush0614.java
+++ src/ex6/rush0614.java
@@ -1,29 +1,46 @@
 package ex6;
+import sun.tools.tree.NewArrayExpression;
+
 import java.util.ArrayList;
 
 public class rush0614 {
     //напишите тут ваш код
-
     public static ArrayList<Cat> cats = new ArrayList<>();
 
-   // public Cat() {
+    public rush0614() {
 
-    //}
+    }
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        Cat mur = new Cat();
+        Cat vaska = new Cat();
+        Cat murka = new Cat();
+        Cat ruzik = new Cat();
+        Cat matros = new Cat();//5
+        Cat pushok = new Cat();
+        Cat barsik = new Cat();
+        Cat kuzia = new Cat();
+        Cat kasi = new Cat();
+        Cat marusia = new Cat();
 
-        for (int i = 0; i < 10; i++) {
-            new Cat();
-            cats.add(new Cat());
-        }
+        cats.add(mur);
+        cats.add(vaska);
+        cats.add(murka);
+        cats.add(ruzik);
+        cats.add(matros);
+        cats.add(pushok);
+        cats.add(barsik);
+        cats.add(kuzia);
+        cats.add(kasi);
+        cats.add(marusia);
         printCats();
     }
 
     public static void printCats() {
         //напишите тут ваш код
-        System.out.println(cats);
-
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println(cats.get(i));
+        }
     }
 }
